74affe9484f7d6c78cab09ac3add08fb4b082499
mbgregory <machon.gregory@gmail.com>
2020-04-16 19:00:50 -0400

    Changed recipe and aggregate rating information to match schema.org

--- recipe_webcrawler/items.py
+++ recipe_webcrawler/items.py
@@ -13,23 +13,21 @@ class RecipeWebcrawlerItem(scrapy.Item):
     # name = scrapy.Field()
     url = scrapy.Field()
     name = scrapy.Field()
-    ingredients = scrapy.Field()
-    instructions = scrapy.Field()
-    servings = scrapy.Field()
+    recipeIngredient = scrapy.Field()
+    recipeInstructions = scrapy.Field()
+    recipeYield = scrapy.Field()
     calories = scrapy.Field()
     fat = scrapy.Field()
     protein = scrapy.Field()
     cholesterol = scrapy.Field()
     sodium = scrapy.Field()
-    categories = scrapy.Field()
-    rating = scrapy.Field()
-    review_cnt = scrapy.Field()
-
+    recipeCategory = scrapy.Field()
+    aggregateRating = scrapy.Field()
     
     def fields_populated(self):
         populated = True
         for field in self.fields:
-            if not field in self.keys() and field != 'servings':
+            if not field in self.keys():
                 populated = False
                 break
         return populated
--- recipe_webcrawler/spiders/allrecipes_spider.py
+++ recipe_webcrawler/spiders/allrecipes_spider.py
@@ -19,7 +19,7 @@ class AllRecipesSpider(scrapy.Spider):
             'https://www.allrecipes.com/recipe/18324/roast-potatoes',
             'https://www.allrecipes.com/recipe/67952/roasted-brussels-sprouts/',
             'https://www.allrecipes.com/recipe/14399/the-best-chicken-salad-ever',
-            'https://www.allrecipes.com/recipes/',
+            #'https://www.allrecipes.com/recipes/',
         ]
 
         for url in urls:
@@ -108,7 +108,7 @@ class AllRecipesSpider(scrapy.Spider):
             rating = float(rating_str)
         if rating_scale_str:
             rating_scale = float(rating_scale_str)
-        return {'value':rating, 'scale':rating_scale}
+        return {'ratingValue':rating, 'bestRating':rating_scale}
 
     def __get_review_cnt(self, response):
         review_re = re.compile(r'(?P<count>\d+)k')
@@ -138,6 +138,7 @@ class AllRecipesSpider(scrapy.Spider):
 
     def parse(self, response):
         recipe_item = RecipeWebcrawlerItem() 
+
         recipe_item['url'] = response.url
 
         json_str = self.__get_recipe_json(response)
@@ -145,46 +146,47 @@ class AllRecipesSpider(scrapy.Spider):
             rp = RecipeParser(json_str)
             if rp.completed():                 
                 recipe_item['name'] = rp.get_recipe_name()
-                recipe_item['ingredients'] = rp.get_recipe_ingredient()
-                recipe_item['instructions'] = rp.get_recipe_instructions()
-                recipe_item['servings'] = rp.get_recipe_yield()
+                recipe_item['recipeIngredient'] = rp.get_recipe_ingredient()
+                recipe_item['recipeInstructions'] = rp.get_recipe_instructions()
+                recipe_item['recipeYield'] = rp.get_recipe_yield()
                 nutrition = rp.get_nutrition()
                 recipe_item['calories'] = nutrition['calories']['quantity']
                 recipe_item['fat'] = nutrition['fatContent']['quantity']
                 recipe_item['protein'] = nutrition['proteinContent']['quantity']
                 recipe_item['cholesterol'] = nutrition['cholesterolContent']['quantity']
                 recipe_item['sodium'] = nutrition['sodiumContent']['quantity']
-                recipe_item['categories'] = rp.get_recipe_category()
-                recipe_item['rating'] = rp.get_rating()
-                recipe_item['review_cnt'] = rp.get_review_cnt()
+                recipe_item['recipeCategory'] = rp.get_recipe_category()
+                recipe_item['aggregateRating'] = rp.get_rating()
+                recipe_item['aggregateRating'].update({'reviewCount':rp.get_review_cnt()})
         else:
             recipe_item['name'] = self.__get_name(response)
-            recipe_item['ingredients'] = self.__get_ingredients(response)
-            recipe_item['instructions'] = self.__get_instructions(response)
-            recipe_item['servings'] = self.__get_servings(response)
+            recipe_item['recipeIngredient'] = self.__get_ingredients(response)
+            recipe_item['recipeInstructions'] = self.__get_instructions(response)
+            recipe_item['recipeYield'] = self.__get_servings(response)
             recipe_item['calories'] = self.__get_calories(response)
             recipe_item['fat'] = self.__get_fat(response)
             recipe_item['protein'] = self.__get_protein(response)
             recipe_item['cholesterol'] = self.__get_cholesterol(response)
             recipe_item['sodium'] = self.__get_sodium(response)
-            recipe_item['categories'] = self.__get_categories(response)
-            recipe_item['rating'] = self.__get_rating(response)
-            recipe_item['review_cnt'] = self.__get_review_cnt(response)
+            recipe_item['recipeCategory'] = self.__get_categories(response)
+            recipe_item['aggregateRating'] = self.__get_rating(response)
+            recipe_item['aggregateRating'].update({'reviewCount':self.__get_review_cnt(response)})
 
       
-        if 'servings' not in recipe_item: 
-            recipe_item['servings'] = self.__get_recipe_yield(response)
-        elif recipe_item['servings'] == None:
-            recipe_item['servings'] = self.__get_recipe_yield(response)
+        if 'recipeYield' not in recipe_item: 
+            recipe_item['recipeYield'] = self.__get_recipe_yield(response)
+        elif recipe_item['recipeYield'] == None:
+            recipe_item['recipeYield'] = self.__get_recipe_yield(response)
             
- 
+
+        #self.logger.info(recipe_item) 
         if recipe_item.fields_populated():
             yield recipe_item
 
         allow_re = ['https://www.allrecipes\.com/recipe.*']
         deny_re = ['https://www.allrecipes\.com/recipe/\d+/.*/reviews/.*','https://www.allrecipes\.com/recipe/\d+/.*/photos/.*']
         links = LinkExtractor(allow=allow_re, deny=deny_re, allow_domains=['allrecipes.com'], unique=True).extract_links(response)
-        for link in links:
-            if link is not None:
-                yield response.follow(link, callback=self.parse)
+        #for link in links:
+        #    if link is not None:
+        #        yield response.follow(link, callback=self.parse)
 
